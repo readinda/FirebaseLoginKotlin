7f182e42a95e130c792308a119a5dce6d88e8447
readinda <adindaadeka48@gmail.com>
2019-11-17 23:30:04 +0700

    add firebase

--- app/src/main/java/com/adindaef/mycodefirebase/MainActivity.kt
+++ app/src/main/java/com/adindaef/mycodefirebase/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,15 +22,24 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_LONG).show()
                 return@setOnClickListener
             }
-            if (email == "admin@gmail.com" && password == "admin"){
-                val progressDialog = ProgressDialog(this, R.style.Theme_MaterialComponents_Light_Dialog)
-                progressDialog.isIndeterminate = true
-                progressDialog.setMessage("Loading")
-                progressDialog.show()
-                val intent = Intent(this, Dashboard::class.java )
-                startActivity(intent)
-                finish()
-            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+
+                    if (!it.isSuccessful){ return@addOnCompleteListener
+                        val intent = Intent (this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, Dashboard::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+
+
+                }
         }
     }
 }
